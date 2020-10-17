db861148721d5d8c48e90edb355f186a73e6079c
Siddharth Singh <sidharth_0903@hotmail.com>
2020-10-17 15:14:38 +0530

    Generate grammar modified
    Now outputs a list of detected terminals and non terminals

--- util/generate_grammar.py
+++ util/generate_grammar.py
@@ -5,6 +5,7 @@ generate_grammar.py --- This module converts the grammar written in easy to read
 import argparse
 import os
 import re
+from pprint import pprint
 
 parser = argparse.ArgumentParser(
     description="This module converts the grammar written in easy to read format (BNF) to a more parsable format"
@@ -37,6 +38,8 @@ def parse_file(input_file_location, output_file_location, delim=" "):
     non_term_regex = re.compile("<(.*?)>")
     input_file = open(input_file_location)
     output_file = open(output_file_location, "w")
+    non_terminals = set()
+    terminals = set()
     for line in input_file:
         line = line.strip()
         if (len(line)) <= 0:
@@ -45,13 +48,19 @@ def parse_file(input_file_location, output_file_location, delim=" "):
         line_wo_comment = non_term_regex.sub("\\1", line_wo_comment)
         line_split = line_wo_comment.split("->")
         non_term = line_split[0].strip()
+        non_terminals.add(non_term)
         rhs = line_split[1].strip()
+        terminals = terminals.union(rhs.split())
         for rhs_tok in rhs.split("|"):
             rhs_tok.strip()
             output_file.writelines("{}{}{}\n".format(non_term, delim, rhs_tok))
+    terminals = terminals.difference(non_terminals)
+    print("Terminals :")
+    print(terminals)
+    print("Non_Terminals :")
+    print(non_terminals)
 
 
 if __name__ == "__main__":
     args = parser.parse_args()
-    print(args)
     parse_file(args.input_file, args.output_file)
