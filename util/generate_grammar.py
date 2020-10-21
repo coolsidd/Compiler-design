cb49758add04a896be387f134d5f3e0eab1528a0
Siddharth Singh <sidharth_0903@hotmail.com>
2020-10-21 23:45:27 +0530

    Modified generate_grammar to read json
    Terminals are now converted from strings using json

--- util/generate_grammar.py
+++ util/generate_grammar.py
@@ -5,6 +5,7 @@ generate_grammar.py --- This module converts the grammar written in easy to read
 import argparse
 import os
 import re
+import json
 from pprint import pprint
 
 parser = argparse.ArgumentParser(
@@ -19,12 +20,20 @@ parser.add_argument(
 parser.add_argument(
     "--grammar-struct-out",
     metavar="Grammar output file",
-        dest="output_grammar_structs",
+    dest="output_grammar_structs",
     type=str,
     help="outputs grammar structures locations",
-    default="./output_file_structs"
+    default="./output_file_structs",
 )
 
+parser.add_argument(
+    "--json_in",
+    dest="json_in",
+    metavar="json input file",
+    type=str,
+    default="symbols.json",
+    help="Input json for terminal/non-terminals to chars",
+)
 parser.add_argument(
     "-o",
     dest="output_file",
@@ -43,7 +52,14 @@ parser.add_argument(
 )
 
 
-def parse_file(input_file_location, output_file_location, delim=" ", generate_c_structs=True, c_structs_location=None):
+def parse_file(
+    input_file_location,
+    output_file_location,
+    delim=" ",
+    generate_c_structs=True,
+    c_structs_location=None,
+    json_in=None,
+):
     non_term_regex = re.compile("<(.*?)>")
     input_file = open(input_file_location)
     output_file = open(output_file_location, "w")
@@ -73,17 +89,45 @@ def parse_file(input_file_location, output_file_location, delim=" ", generate_c_
     print("Non_Terminals :")
     print(sorted(non_terminals))
     if generate_c_structs:
-        output_file_structs = open(c_structs_location+".h", "w")
-        output_file_structs.write("""/* Header guard */\n#ifndef GRAMMAR_H\n#define GRAMMAR_H\n/***************/\n#include <stdio.h>\n#include <string.h>\ntypedef enum {{ false, true }} bool;\n\ntypedef enum {{ // list all the non terminals or terminals\n{}\n}} BaseSymbol;\n\ntypedef struct symbol {{ // symbol with info if it is terminal or not\n    bool is_terminal;\n    BaseSymbol s;\n}} Symbol;\n\nSymbol toSymbol(char *enustr);\nvoid printSymbol(Symbol symb);\n\n#endif\n""".format("\n".join(["    "+x+"," for x in sorted(non_terminals)]+["    "+x+"," for x in sorted(terminals)])))
+        output_file_structs = open(c_structs_location + ".h", "w")
+        output_file_structs.write(
+            """/* Header guard */\n#ifndef GRAMMAR_H\n#define GRAMMAR_H\n/***************/\n#include <stdio.h>\n#include <string.h>\ntypedef enum {{ false, true }} bool;\n\ntypedef enum {{ // list all the non terminals or terminals\n{}\n}} BaseSymbol;\n\ntypedef struct symbol {{ // symbol with info if it is terminal or not\n    bool is_terminal;\n    BaseSymbol s;\n}} Symbol;\n\nSymbol toSymbol(char *enustr);\nvoid printSymbol(Symbol symb);\n\n#endif\n""".format(
+                "\n".join(
+                    ["    " + x + "," for x in sorted(non_terminals)]
+                    + ["    " + x + "," for x in sorted(terminals)]
+                )
+            )
+        )
         output_file_structs.close()
-        with open(c_structs_location+".c", "w") as output_file_structs:
-            template_str_toSymbol = """    if (strcmp(enustr, "{0}") == 0) {{\n        ans.is_terminal = {1};\n        ans.s = {0};\n        return ans;\n    }}\n"""
-            template_filled_toSymbol = [template_str_toSymbol.format(x, "true") for x in terminals]+[template_str_toSymbol.format(x, "false") for x in non_terminals]
-            template_str_print = """    case ({0}):\n        printf("{0}\\n");\n        break;"""
-            template_filled_print = [template_str_print.format(x) for x in list(sorted(terminals))+list(sorted(non_terminals))]
-            output_file_structs.write("""#include "./output_file_structs.h"\n#include "./other_structs.h"\n#include <stdio.h>\n#include <string.h>\n\nSymbol toSymbol(char *enustr) {{\n    Symbol ans;\n{}\n    return ans;\n}}\nvoid printSymbol(Symbol symb) {{\n    printf("Symbol variable : ");\n    switch (symb.s) {{\n{}\n    }}\n    printf("    is_terminal : %s\\n", symb.is_terminal ? "true" : "false");\n}}\n""".format("\n".join(template_filled_toSymbol),"\n".join(template_filled_print)));
+        json_input_file = open(json_in)
+        terminals_json = json.load(json_input_file)
+        json_input_file.close()
+        with open(c_structs_location + ".c", "w") as output_file_structs:
+            template_str_toSymbol = """    if (strcmp(enustr, "{2}") == 0) {{\n        ans.is_terminal = {1};\n        ans.s = {0};\n        return ans;\n    }}\n"""
+            template_filled_toSymbol = [
+                template_str_toSymbol.format(x, "true", terminals_json[x])
+                for x in terminals
+            ] + [template_str_toSymbol.format(x, "false", x) for x in non_terminals]
+            template_str_print = (
+                """    case ({0}):\n        printf("{0}\\n");\n        break;"""
+            )
+            template_filled_print = [
+                template_str_print.format(x)
+                for x in list(sorted(terminals)) + list(sorted(non_terminals))
+            ]
+            output_file_structs.write(
+                """#include "./output_file_structs.h"\n#include "./other_structs.h"\n#include <stdio.h>\n#include <string.h>\n\nSymbol toSymbol(char *enustr) {{\n    Symbol ans;\n{}\n    return ans;\n}}\nvoid printSymbol(Symbol symb) {{\n    printf("Symbol variable : ");\n    switch (symb.s) {{\n{}\n    }}\n    printf("    is_terminal : %s\\n", symb.is_terminal ? "true" : "false");\n}}\n""".format(
+                    "\n".join(template_filled_toSymbol),
+                    "\n".join(template_filled_print),
+                )
+            )
 
 
 if __name__ == "__main__":
     args = parser.parse_args()
-    parse_file(args.input_file, args.output_file, c_structs_location=args.output_grammar_structs)
+    parse_file(
+        args.input_file,
+        args.output_file,
+        c_structs_location=args.output_grammar_structs,
+        json_in=args.json_in
+    )
