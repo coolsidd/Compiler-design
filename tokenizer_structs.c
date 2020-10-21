493cdc332e4967a9d8c35a1e03d81aa1a212bd2a
Siddharth Singh <sidharth_0903@hotmail.com>
2020-10-21 23:46:14 +0530

    Tokenizer almost done (Testing left)

--- tokenizer_structs.c
+++ tokenizer_structs.c
@@ -1,24 +1,68 @@
-#include "tokenizer_struct.h"
+#include "tokenizer_structs.h"
 #include <stdlib.h>
 #include <string.h>
-
-Symbol toLex(char* lex_str){
-    Symbol symb;
-    symb.is_terminal=true;
-    if(strcmp(lex_str, "{")){
-        symb.s=CURLYOPEN;
-        return symb;
-    }
-}
 
 TokenStream *insertIntoStream(TokenStream *s, int line_num, char *token_str) {
     Token *newToken = (Token *)malloc(sizeof(Token));
     newToken->line_no = line_num;
     newToken->token_name = token_str;
-    newToken->lexeme = toLex(char *token_str);
-    s->tail->next = newToken;
+    newToken->lexeme = toSymbol(token_str);
+    newToken->next = NULL;
+    if (s->tail) {
+        s->tail->next = newToken;
+        s->length++;
+        return s;
+    }
+    s->head = newToken;
+    s->tail = newToken;
     s->length++;
     return s;
 }
-TokenStream *newTokenStream();
-void freeTokenStream(TokenStream *s);
+
+TokenStream *newTokenStream() {
+    TokenStream *s = (TokenStream *)malloc(sizeof(TokenStream));
+    s->head = NULL;
+    s->tail = NULL;
+    s->length = 0;
+    return s;
+}
+void freeTokenStream(TokenStream *s){
+    while(s->head){
+        Token *temp = s->head->next;
+        free(s->head);
+        s->head = temp;
+    }
+
+}
+void deleteHead(TokenStream *s){
+    if(!s->head){
+        printf("ERROR DELETING HEAD OF SIZE 0");
+        return;
+    }
+    Token *temp = s->head->next;
+    free(s->head);
+    s->head = temp;
+    s->length--;
+}
+
+void deleteTail(TokenStream *s){
+    if(!s->head){
+        printf("ERROR DELETING TAIL OF SIZE 0");
+        return;
+    }
+    s->length--;
+    Token *temp = s->head;
+    if(!temp->next){
+        free(temp);
+        s->head = NULL;
+        s->tail = NULL;
+        return;
+    }
+    while(temp->next->next){
+        temp = temp->next;
+    }
+    s->tail = temp;
+    free(temp->next);
+    s->tail->next = NULL;
+
+}
