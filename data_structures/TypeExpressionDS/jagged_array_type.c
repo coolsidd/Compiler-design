821720cfbe97911a4ddc1791ed6c48dad61a82d4
adityatulsyan <tulsyanaditya6@gmail.com>
2020-10-26 12:21:54 +0530

    JaggedArray modifications

--- data_structures/TypeExpressionDS/jagged_array_type.c
+++ data_structures/TypeExpressionDS/jagged_array_type.c
@@ -1,20 +1,20 @@
 #include "jagged_array_type.h"
 
 jagged_2d* create_jagged_2d(){
-    jagged_2d *j2d = (jagged_2d *)calloc(1, sizeof(jagged_2d));
+    jagged_2d *j2d = (jagged_2d*)calloc(1, sizeof(jagged_2d));
     j2d->row_sizes = create_r2_dimension();
     return j2d;
 }
 
 jagged_3d* create_jagged_3d(){
-    jagged_3d *j3d = (jagged_3d *)calloc(1, sizeof(jagged_3d));
+    jagged_3d *j3d = (jagged_3d*)calloc(1, sizeof(jagged_3d));
     j3d->row_sizes = create_linked_list();
     return j3d;
 }
 r2_dimension create_r2_dimension(){
-    r2_dimension r2 = (r2_dimension) calloc(1, sizeof(r2_dimension));
-    r2.sizes = create_linked_list();
-    return r2;
+    r2_dimension* r2 = (r2_dimension*)calloc(1, sizeof(r2_dimension));
+    r2->sizes = create_linked_list();
+    return *r2;
 }
 
 void append_size(linked_list *ll, int size){
