908e5b023e6ddf13fdd9b52220fa5ebe9126f297
Nayan Khanna <f20170636@pilani.bits-pilani.ac.in>
2020-10-27 16:09:22 +0530

    Added assignment helpers function prototypes to header

--- data_structures/TypeExpressionDS/assign_helpers.h
+++ data_structures/TypeExpressionDS/assign_helpers.h
@@ -5,5 +5,12 @@
 #include "../../final_code/parse_tree.h"
 
 type_expression *get_type_of_fact(type_exp_table *txp_table, Parse_tree_node *p);
+type_expression *get_integer_type();
+type_expression *get_real_type();
+type_expression *get_bool_type();
+type_expression *get_type_of_arithm_expr(type_exp_table *txp_table, Parse_tree_node *p);
+type_expression *get_type_of_term(type_exp_table *txp_table, Parse_tree_node *p);
+bool check_rect_dimensions(rect_array_type r1, rect_array_type r2);
+bool check_jagged_dimensions(jagged_array_type j1, jagged_array_type j2);
 
 #endif
