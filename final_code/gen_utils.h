585181c69fdebf3d90dd2d5dd9772245ed6c9f5f
adityatulsyan <tulsyanaditya6@gmail.com>
2020-10-28 02:39:02 +0530

    Modified error funtions

--- final_code/gen_utils.h
+++ final_code/gen_utils.h
@@ -8,9 +8,20 @@
 #include "parse_tree.h"
 #include "./type_errors.h"
 #include "grammar_structs.h"
+#include "../data_structures/linked_list.h"
 #include <stdlib.h>
 #include <math.h>
+
+typedef struct en
+{
+    int line_num, depth;
+    char *stmt_type, *op, *message;
+    char *lex1, *lex2, *type1, *type2;
+} ErrorNode;
+
 
 void assert(bool condition, char *error_string);
 bool assert_debug(bool condition, char* error_string, Parse_tree_node * p, char* t1, char* t2, char* operator, char* lex1, char* lex2);
 #endif
+
+
--- final_code/type_errors.c
+++ final_code/type_errors.c
@@ -1,49 +1,49 @@
-#include "./type_errors.h"
+// #include "./type_errors.h"
 
 
-void printErrors(ErrorNode* err) {
-    printf( "|%5s|%10s|%10s|%10s|%10s|%10s|%10s|%5s|%20s|\n",
-            "num", "stmt type", "operator", "lexeme 1", "type 1",
-            "lexeme 2", "type 2", "depth", "error message");
+// void printErrors(ErrorNode* err) {
+//     printf( "|%5s|%10s|%10s|%10s|%10s|%10s|%10s|%5s|%20s|\n",
+//             "num", "stmt type", "operator", "lexeme 1", "type 1",
+//             "lexeme 2", "type 2", "depth", "error message");
 
-    while (err != NULL) {
-        printf( "|%5d|%10s|%10s|%10s|%10s|%10s|%10s|%5d|%20s|\n",
-                err->line_num, err->stmt_type, err->op, err->lex1, err->type1,
-                err->lex2, err->type2,  err->depth, err->message);
-        err = err->next;
-    }
-}
+//     while (err != NULL) {
+//         printf( "|%5d|%10s|%10s|%10s|%10s|%10s|%10s|%5d|%20s|\n",
+//                 err->line_num, err->stmt_type, err->op, err->lex1, err->type1,
+//                 err->lex2, err->type2,  err->depth, err->message);
+//         err = err->next;
+//     }
+// }
 
-ErrorNode* add_error( ErrorNode* err,
-                char* lex1, char* type1, char* lex2, char* type2,
-                int ln, int depth, char* stmt_type, char* op, char* message) {
+// ErrorNode* add_error( ErrorNode* err,
+//                 char* lex1, char* type1, char* lex2, char* type2,
+//                 int ln, int depth, char* stmt_type, char* op, char* message) {
 
-    ErrorNode* newerr = (ErrorNode*)malloc(sizeof(ErrorNode));
-    newerr->lex1 = lex1;
-    newerr->lex2 = lex2;
-    newerr->type1 = type1;
-    newerr->type2 = type2;
-    newerr->line_num = ln;
-    newerr->depth = depth;
-    newerr->stmt_type = stmt_type;
-    newerr->op = op;
-    newerr->message = message;
+//     ErrorNode* newerr = (ErrorNode*)malloc(sizeof(ErrorNode));
+//     newerr->lex1 = lex1;
+//     newerr->lex2 = lex2;
+//     newerr->type1 = type1;
+//     newerr->type2 = type2;
+//     newerr->line_num = ln;
+//     newerr->depth = depth;
+//     newerr->stmt_type = stmt_type;
+//     newerr->op = op;
+//     newerr->message = message;
 
-    if (!err) {
-        err = newerr;
-    } else {
-        err->next = newerr;
-        err = newerr;
-    }
+//     if (!err) {
+//         err = newerr;
+//     } else {
+//         err->next = newerr;
+//         err = newerr;
+//     }
 
-    return err;
-}
+//     return err;
+// }
 
-void destroy_err(ErrorNode* err) {
-   ErrorNode* tmp;
-   while (err != NULL) {
-       tmp = err;
-       err = err->next;
-       free(tmp);
-    }
-}
+// void destroy_err(ErrorNode* err) {
+//    ErrorNode* tmp;
+//    while (err != NULL) {
+//        tmp = err;
+//        err = err->next;
+//        free(tmp);
+//     }
+// }
